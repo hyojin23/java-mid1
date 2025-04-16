78c59c8b952d67c3c5236b6c85ca651c568764d9
hyojin23 <kimhj9292@gmail.com>
2025-04-16 18:10:01 +0900

    String 클래스 - 불변 객체까지

---
+++ src/lang/immutable/test/ImmutableMyDataMain.java
@@ -0,0 +1,18 @@
+package lang.immutable.test;
+
+public class ImmutableMyDataMain {
+
+    public static void main(String[] args) {
+
+        //date1=x001, date2=x001
+        ImmutableMyDate date1 = new ImmutableMyDate(2024, 1, 1);
+        ImmutableMyDate date2 = date1;
+        System.out.println("date1 = " + date1);
+        System.out.println("date2 = " + date2);
+
+        System.out.println("2025 -> date1");
+        date1 = date1.withYear(2025);   //x002
+        System.out.println("date1 = " + date1); //date1=x002
+        System.out.println("date2 = " + date2); //date2=x001
+    }
+}
